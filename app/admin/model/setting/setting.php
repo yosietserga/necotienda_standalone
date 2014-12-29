9ea22c6d95f1b9bc1f11707b7484e515c16546bc
yosietserga <yosietserga@gmail.com>
2014-12-29 02:12:54 -0430

    Filemanager updated, use fancybox now

--- app/admin/model/setting/setting.php
+++ app/admin/model/setting/setting.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /**
  * ModelSettingSetting
  * 
@@ -9,83 +10,87 @@
  * @access public
  */
 class ModelSettingSetting extends Model {
-	public function getSetting($group, $store_id=0) {
-		$data = array();
-		$query = $this->db->query("SELECT * 
+
+    public function getSetting($group, $store_id = 0) {
+        $data = array();
+        $query = $this->db->query("SELECT * 
         FROM " . DB_PREFIX . "setting 
         WHERE `group` = '" . $this->db->escape($group) . "' 
-        AND store_id = '". (int)$store_id ."'");
-		foreach ($query->rows as $result) {
-			$data[$result['key']] = $result['value'];
-		}
-		return $data;
-	}
-	
-	public function update($group, $data, $store_id=0) {
-		$this->db->query("DELETE 
+        AND store_id = '" . (int) $store_id . "'");
+        foreach ($query->rows as $result) {
+            $data[$result['key']] = $result['value'];
+        }
+        return $data;
+    }
+
+    public function update($group, $data, $store_id = 0) {
+        $this->db->query("DELETE 
         FROM " . DB_PREFIX . "setting 
         WHERE `group` = '" . $this->db->escape($group) . "' 
-        AND store_id = '". (int)$store_id ."'");
-        
-		foreach ($data as $key => $value) {		  
-		if ($key == 'config_bounce_password' && !empty($value)) $value = base64_encode($value); 
-		if ($key == 'config_smtp_password' && !empty($value)) $value = base64_encode($value); 
-			$this->db->query("INSERT INTO " . DB_PREFIX . "setting SET `group` = '" . $this->db->escape($group) . "', `key` = '" . $this->db->escape($key) . "', `value` = '" . $this->db->escape($value) . "'");
-		}
-	}
-    
-	public function updateProperty($group, $key, $data, $store_id=0) {
-		$this->db->query("REPLACE INTO " . DB_PREFIX . "setting SET 
+        AND store_id = '" . (int) $store_id . "'");
+
+        foreach ($data as $key => $value) {
+            if ($key == 'config_bounce_password' && !empty($value))
+                $value = base64_encode($value);
+            if ($key == 'config_smtp_password' && !empty($value))
+                $value = base64_encode($value);
+            $this->db->query("INSERT INTO " . DB_PREFIX . "setting SET `group` = '" . $this->db->escape($group) . "', `key` = '" . $this->db->escape($key) . "', `value` = '" . $this->db->escape($value) . "'");
+        }
+    }
+
+    public function updateProperty($group, $key, $data, $store_id = 0) {
+        $this->db->query("REPLACE INTO " . DB_PREFIX . "setting SET 
         `group` = '" . $this->db->escape($group) . "',
-        `key`   = '". $this->db->escape($key) ."',
+        `key`   = '" . $this->db->escape($key) . "',
         `value` = '" . $this->db->escape($data) . "',
-        `store_id` = '". (int)$store_id ."'");
-	}
-    
-    public function editMaintenance($data,$group = 'config', $store_id=0) {		
-    	$this->db->query("UPDATE " . DB_PREFIX . "setting SET 
+        `store_id` = '" . (int) $store_id . "'");
+    }
+
+    public function editMaintenance($data, $group = 'config', $store_id = 0) {
+        $this->db->query("UPDATE " . DB_PREFIX . "setting SET 
         `value` = '" . $this->db->escape($data) . "' 
         WHERE `key` = 'config_maintenance' 
-        AND store_id = '". (int)$store_id ."'");
-	}
-	
-	public function delete($group, $store_id=0) {
-		$this->db->query("DELETE FROM " . DB_PREFIX . "setting 
+        AND store_id = '" . (int) $store_id . "'");
+    }
+
+    public function delete($group, $store_id = 0) {
+        $this->db->query("DELETE FROM " . DB_PREFIX . "setting 
         WHERE `group` = '" . $this->db->escape($group) . "' 
-        AND store_id = '". (int)$store_id ."'");
-	}
-    
-	/**
-	 * ModelCatalogProduct::sortProduct()
-	 * @param array $data
+        AND store_id = '" . (int) $store_id . "'");
+    }
+
+    /**
+     * ModelCatalogProduct::sortProduct()
+     * @param array $data
      * @see DB
      * @see Cache
-	 * @return void
-	 */
-	public function sortExtensions($data, $store_id=0) {
-	   //TODO: acomodar para que ordene por módulo y por posición del módulo
-	   /*
-	   if (!is_array($data)) return false;
-       $left = $this->db->query("SELECT * FROM " . DB_PREFIX . "setting WHERE `value` = 'left'");
-       $right = $this->db->query("SELECT * FROM " . DB_PREFIX . "setting WHERE `value` = 'left'");
-       $center = $this->db->query("SELECT * FROM " . DB_PREFIX . "setting WHERE `value` = 'left'");
-       
-       if ($left->num_rows) {
-            foreach ($data as $k => $v) {
-                if ($v['position'] != 'left') continue;
-                if ($v['position'] != 'left') continue;
-                
-            }
-       }
-       **/
-       $pos = 1;
-       foreach ($data as $v) {
+     * @return void
+     */
+    public function sortExtensions($data, $store_id = 0) {
+        //TODO: acomodar para que ordene por mï¿½dulo y por posiciï¿½n del mï¿½dulo
+        /*
+          if (!is_array($data)) return false;
+          $left = $this->db->query("SELECT * FROM " . DB_PREFIX . "setting WHERE `value` = 'left'");
+          $right = $this->db->query("SELECT * FROM " . DB_PREFIX . "setting WHERE `value` = 'left'");
+          $center = $this->db->query("SELECT * FROM " . DB_PREFIX . "setting WHERE `value` = 'left'");
+
+          if ($left->num_rows) {
+          foreach ($data as $k => $v) {
+          if ($v['position'] != 'left') continue;
+          if ($v['position'] != 'left') continue;
+
+          }
+          }
+         * */
+        $pos = 1;
+        foreach ($data as $v) {
             $this->db->query("UPDATE " . DB_PREFIX . "setting SET 
-            `value` = '" . (int)$pos . "' 
+            `value` = '" . (int) $pos . "' 
             WHERE `key` = '" . $this->db->escape($v['group']) . "_sort_order' 
-            AND store_id = '". (int)$store_id ."'");
+            AND store_id = '" . (int) $store_id . "'");
             $pos++;
-       }
-	   return true;
-	}
+        }
+        return true;
+    }
+
 }
